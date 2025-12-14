28419af4d9d035939b8e46b542574de9e396d7c9
jb9485 <jb7706@outlook.com>
2025-12-13 17:12:13 -0800

    more logging

--- rtl433-us/run.sh
+++ rtl433-us/run.sh
@@ -13,9 +13,7 @@ MQTT_URL="mqtt://$MQTT_HOST:$MQTT_PORT"
 [ -n "$MQTT_USER" ] && MQTT_URL="$MQTT_URL,user=$MQTT_USER"
 [ -n "$MQTT_PASS" ] && MQTT_URL="$MQTT_URL,pass=$MQTT_PASS"
 
-PIDS=()
-
-while IFS= read -r d; do
+jq -c '.dongles[]' $CONFIG | while read -r d; do
     DEVICE=$(echo "$d" | jq -r '.device // "0"')
     FREQ=$(echo "$d" | jq -r '.frequency // 433')
 
@@ -27,14 +25,6 @@ while IFS= read -r d; do
 
     PREFIX="${FREQ}mhz"
 
-    rtl_433 -d "$DEVICE" -f $TUNE -s $RATE -q -C si -M utc \
-        -F "$MQTT_URL,retain=1,devices=rtl_433/${PREFIX}/[model]/[id]" &
-    PIDS+=($!)
-done < <(jq -c '.dongles[]' $CONFIG)
-
-if [ ${#PIDS[@]} -eq 0 ]; then
-    echo "No valid dongles configured - sleeping"
-    sleep infinity
-fi
-
-wait ${PIDS[@]}
+    rtl_433 -d "$DEVICE" -f $TUNE -s $RATE -C si -M utc -F log \
+        -F "$MQTT_URL,retain=1,devices=rtl_433/${PREFIX}/[model]/[id]"
+done
