a3b27b155e6f62168373194c92b40a6645b2f89f
jb9485 <jb7706@outlook.com>
2025-12-19 19:03:29 -0800

    boost the logging a bit

--- rtl433-us/run.sh
+++ rtl433-us/run.sh
@@ -21,4 +21,4 @@ esac
 
 PREFIX="${FREQ}mhz"
 
-rtl_433 -d 0 -f $TUNE -s $RATE -q -F log -F "$MQTT_URL,retain=1,devices=rtl_433/${PREFIX}/[model]/[id]"
+rtl_433 -d 0 -f $TUNE -s $RATE -vv -C si -M utc -F "$MQTT_URL,retain=1,devices=rtl_433/${PREFIX}/[model]/[id]"
